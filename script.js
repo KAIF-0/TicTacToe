f332b5666c40534b003c45d4aff8cdb1a6874592
KAIF 0 <kaif8700979251@gmail.com>
2023-12-09 18:18:11 +0530

    Its a Gane

---
+++ script.js
@@ -0,0 +1,87 @@
+let boxes = document.querySelectorAll(".cell");
+let resetBtn = document.querySelector("#restart-btn");
+let msg = document.querySelector(".game-status");
+
+let turnO = true; 
+let count = 0; 
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msg.textContent = "";
+};
+
+resetBtn.addEventListener("click", resetGame);
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    count++;
+
+    let isWinner = checkWinner();
+
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
+  });
+});
+
+const gameDraw = () => {
+  msg.textContent = `Game was a Draw.`;
+  disableBoxes();
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.textContent = `Congratulations, Winner is ${winner}`;
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        disableBoxes();
+        showWinner(pos1Val);
+        //return true;
+      }
+    }
+  }
+};
+
+
